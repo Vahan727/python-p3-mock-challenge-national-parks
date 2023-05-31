4355f496d984bd4dafd40f6296a7c931aa3bd51c
Vahan Nadjarian <vahan@nadjarian.com>
2023-05-31 10:15:05 -0400

    completed cc

--- lib/classes/trip.py
+++ lib/classes/trip.py
@@ -1,3 +1,5 @@
+from classes.visitor import Visitor
+from classes.national_park import NationalPark
 
 class Trip:
     
@@ -6,3 +8,36 @@ class Trip:
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+
+        national_park.trips(self)
+        national_park.visitors(visitor)
+
+        visitor.trips(self)
+        visitor.national_parks(national_park)
+       
+
+    @property
+    def visitor(self):
+        return self._visitor
+
+    @visitor.setter
+    def visitor(self, visitor):
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+        else:
+            raise Exception("Not a visitor object")
+    
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, national_park):
+        if isinstance(national_park, NationalPark):
+            self._national_park = national_park
+        else:
+            raise Exception("Not a NationalPark object")
+    
+
+    
+    
